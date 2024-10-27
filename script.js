38218753bfc55a8de3478e69db8977c65a5d72e5
JoahansKepler <joahang23@gmail.com>
2024-10-27 13:33:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function Like()
+{
+    alert ("Prueba de mensaje de alerta.")
+}
+
+let nombre = "joahan S. Kepler"
+const edad = "20"
+
+function saludo ()
+{
+    alert ("hola, " + nombre )
+}
+function saludar()
+{
+    let nombre = document.getElementById("nombredeusuario").value;
+    alert("holaaa, " + nombre);
+}
